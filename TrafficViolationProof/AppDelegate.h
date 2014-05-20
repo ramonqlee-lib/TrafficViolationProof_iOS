e0f34ee8cdf8fda8f361b684d5dedaa7d3361331
ramonqlee1980 <ramonqlee1980@gmail.com>
2014-05-21 06:13:23 +0800

    Initial Commit

---
+++ TrafficViolationProof/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrafficViolationProof
+//
+//  Created by ramonqlee on 5/21/14.
+//  Copyright (c) 2014 iDreems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrafficViolationProof/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrafficViolationProof
+//
+//  Created by ramonqlee on 5/21/14.
+//  Copyright (c) 2014 iDreems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
